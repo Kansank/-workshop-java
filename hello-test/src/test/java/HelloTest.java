52f525edef7270513feca0119efbbbba47119257
Kansank.Int <Kansank.Int@NTBLTR9114HP3>
2021-07-23 12:01:55 +0700

    first commit

---
+++ hello-test/src/test/java/HelloTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloTest {
+    // f(name) = "Hello somkiat"
+    @Test
+    @DisplayName("ทำการทดสอบ .....")
+    public void case01() {
+        // Arrange
+        Hello hello = new Hello();
+        String name = "somkiat";
+        // Act
+        String actualResult = hello.sawadee(name);
+        // Assert
+        assertEquals("Hello somkiat", actualResult);
+    }
+}
